54ac1374922dd8c39946dd864a9c732735499860
siwel-cg <lghrist@seas.upenn.edu>
2025-10-17 11:24:42 -0400

    Basic setup of f+ (just copied from naive setup)

--- src/renderers/forward_plus.ts
+++ src/renderers/forward_plus.ts
@@ -6,15 +6,142 @@ export class ForwardPlusRenderer extends renderer.Renderer {
     // TODO-2: add layouts, pipelines, textures, etc. needed for Forward+ here
     // you may need extra uniforms such as the camera view matrix and the canvas resolution
 
+    sceneUniformsBindGroupLayout: GPUBindGroupLayout;
+    sceneUniformsBindGroup: GPUBindGroup;
+
+    depthTexture: GPUTexture;
+    depthTextureView: GPUTextureView;
+
+    pipeline: GPURenderPipeline;
+
     constructor(stage: Stage) {
         super(stage);
 
         // TODO-2: initialize layouts, pipelines, textures, etc. needed for Forward+ here
+
+        // CREATE BIND GROUP "BLUE-PRINT": What is the type of the binding and what can acess that data (doesn't specify the actual data). binding number and visibility is important: MUST MATCH IN SHADER AND BING GROUP INITIALIZATION
+        this.sceneUniformsBindGroupLayout = renderer.device.createBindGroupLayout({
+            label: "scene uniforms bind group layout",
+            entries: [
+                { // CAMERA
+                    binding: 0,
+                    visibility: GPUShaderStage.VERTEX,
+                    buffer: { type: "uniform"}
+                },
+                {   // LIGHTSET
+                    binding: 1,
+                    visibility: GPUShaderStage.FRAGMENT,
+                    buffer: { type: "read-only-storage"}
+                }
+            ]
+        });
+
+        // BINDS ACTUAL CPU DATA TO THESE GPU BUFFERS
+        this.sceneUniformsBindGroup = renderer.device.createBindGroup
+        ({
+            label: "scene uniforms bind group",
+            layout: this.sceneUniformsBindGroupLayout,
+
+            entries: [
+                { // CAMERA
+                    binding: 0,
+                    resource: { buffer: this.camera.uniformsBuffer}
+                },
+                { // LIGHTS
+                    binding: 1,
+                    resource: { buffer: this.lights.lightSetStorageBuffer}
+                }
+            ]
+        });
+
+        // ALOCATE MEMORY FOR DEPTH TEXTURE: size of screen
+        this.depthTexture = renderer.device.createTexture
+        ({
+            size: [renderer.canvas.width, renderer.canvas.height],
+            format: "depth24plus",
+            usage: GPUTextureUsage.RENDER_ATTACHMENT
+        });
+        this.depthTextureView = this.depthTexture.createView();
+        
+
+        this.pipeline = renderer.device.createRenderPipeline({
+
+            // BINDS LAYOUTS TO THE PIPELINE?
+            layout: renderer.device.createPipelineLayout({
+                label: "forward+ pipeline layout",
+                bindGroupLayouts: [
+                    this.sceneUniformsBindGroupLayout,
+                    renderer.modelBindGroupLayout,
+                    renderer.materialBindGroupLayout
+                ]
+            }),
+            depthStencil: { // 
+                depthWriteEnabled: true,
+                depthCompare: "less",
+                format: "depth24plus"
+            },
+            vertex: { // REUSE NAIVE VERTEX SHADER
+                module: renderer.device.createShaderModule({
+                    label: "forward+ vert shader",
+                    code: shaders.naiveVertSrc
+                }),
+                buffers: [ renderer.vertexBufferLayout ]
+            },
+            fragment: { // USE FORWARD+ FRAG SHADER
+                module: renderer.device.createShaderModule({
+                    label: "forward+ frag shader",
+                    code: shaders.forwardPlusFragSrc,
+                }),
+                targets: [
+                    {
+                        format: renderer.canvasFormat,
+                    }
+                ]
+            }
+        }); 
     }
 
     override draw() {
         // TODO-2: run the Forward+ rendering pass:
         // - run the clustering compute shader
         // - run the main rendering pass, using the computed clusters for efficient lighting
+
+        // THIS IS CURRENTLY JUST NAIVE DRAW (NO FORWARD +)
+        const encoder = renderer.device.createCommandEncoder();
+        const canvasTextureView = renderer.context.getCurrentTexture().createView();
+
+        const renderPass = encoder.beginRenderPass({
+            label: "naive render pass",
+            colorAttachments: [
+                {
+                    view: canvasTextureView,
+                    clearValue: [0, 0, 0, 0],
+                    loadOp: "clear",
+                    storeOp: "store"
+                }
+            ],
+            depthStencilAttachment: {
+                view: this.depthTextureView,
+                depthClearValue: 1.0,
+                depthLoadOp: "clear",
+                depthStoreOp: "store"
+            }
+        });
+        renderPass.setPipeline(this.pipeline);
+        renderPass.setBindGroup(shaders.constants.bindGroup_scene, this.sceneUniformsBindGroup);
+        
+        this.scene.iterate(node => {
+            renderPass.setBindGroup(shaders.constants.bindGroup_model, node.modelBindGroup);
+        }, material => {
+            renderPass.setBindGroup(shaders.constants.bindGroup_material, material.materialBindGroup);
+        }, primitive => {
+            renderPass.setVertexBuffer(0, primitive.vertexBuffer);
+            renderPass.setIndexBuffer(primitive.indexBuffer, 'uint32');
+            renderPass.drawIndexed(primitive.numIndices);
+        });
+
+        renderPass.end();
+
+        renderer.device.queue.submit([encoder.finish()]);
     }
 }
--- src/shaders/naive.fs.wgsl
+++ src/shaders/naive.fs.wgsl
@@ -7,7 +7,7 @@ struct FragmentInput
 {
     @location(0) pos: vec3f,
     @location(1) nor: vec3f,
-    @location(2) uv: vec2f
+    @location(2) uv: vec2f,
 }
 
 @fragment
